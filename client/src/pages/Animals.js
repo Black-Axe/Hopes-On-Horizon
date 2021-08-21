3b649d953eb9a5c89408c2103d95430d6073aa72
sherief <elsowiny@yahoo.com>
2021-08-21 16:00:07 -0400

    build add animal page

---
+++ client/src/pages/Animals.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Animals from '../components/Animals/Animals'
+export default function SearchAnimals() {
+    return (
+        <>
+        <Banner />
+        <Animals />
+
+
+        </>
+    )
+}
