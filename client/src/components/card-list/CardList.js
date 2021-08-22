33aefa1ed97f0cd3a2eda6e98c6185d30e6d3c11
elsowiny <elsowiny@yahoo.com>
2021-08-21 23:52:44 -0400

    styles

--- client/src/components/card-list/CardList.js
+++ client/src/components/card-list/CardList.js
@@ -41,14 +41,14 @@ export default function CardList({animals}) {
 
 
         animals ?
-        (<>
+        (<div className="card-list-container">
         <div className='card-list'>
             {animals.map((animal, index) => (
                 <Card key={index} animal={animal} />
             ))}
 
         </div>
-        </>)
+        </div>)
         :
         (<div></div>)
     )
