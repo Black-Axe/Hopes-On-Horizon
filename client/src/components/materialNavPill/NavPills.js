5f6a8e4d3f44f38ae11b9c362538640083722f6b
Sherief Elsowiny <elsowiny@yahoo.com>
2021-09-04 20:21:10 -0400

    add profle page for animal / fix styling to use absolute import which fixes bug of pages not rendering the css for nested routes

---
+++ client/src/components/materialNavPill/NavPills.js
@@ -0,0 +1,123 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+
+// core components
+import GridContainer from "../materialGridContainer/GridContainer";
+import GridItem from "../materialGridItem/GridItem";
+
+import styles from "./navPillsStyle";
+
+const useStyles = makeStyles(styles);
+
+export default function NavPills(props) {
+  const [active, setActive] = React.useState(props.active);
+  const handleChange = (event, active) => {
+    setActive(active);
+  };
+  const { tabs, color, horizontal, alignCenter } = props;
+  const classes = useStyles();
+  const flexContainerClasses = classNames({
+    [classes.flexContainer]: true,
+    [classes.horizontalDisplay]: horizontal !== undefined,
+  });
+  const tabButtons = (
+    <Tabs
+      classes={{
+        root: classes.root,
+        fixed: classes.fixed,
+        flexContainer: flexContainerClasses,
+        indicator: classes.displayNone,
+      }}
+      value={active}
+      onChange={handleChange}
+      centered={alignCenter}
+    >
+      {tabs.map((prop, key) => {
+        var icon = {};
+        if (prop.tabIcon !== undefined) {
+          icon["icon"] = <prop.tabIcon className={classes.tabIcon} />;
+        }
+        const pillsClasses = classNames({
+          [classes.pills]: true,
+          [classes.horizontalPills]: horizontal !== undefined,
+          [classes.pillsWithIcons]: prop.tabIcon !== undefined,
+        });
+        return (
+          <Tab
+            label={prop.tabButton}
+            key={key}
+            {...icon}
+            classes={{
+              root: pillsClasses,
+              label: classes.label,
+              selected: classes[color],
+            }}
+          />
+        );
+      })}
+    </Tabs>
+  );
+  const tabContent = (
+    <div className={classes.contentWrapper}>
+      {tabs.map((prop, key) => {
+        if (key === active) {
+          return (
+            <div className={classes.tabContent} key={key}>
+              {prop.tabContent}
+            </div>
+          );
+        }
+        return null;
+      })}
+    </div>
+  );
+  return horizontal !== undefined ? (
+    <GridContainer>
+      <GridItem {...horizontal.tabsGrid}>{tabButtons}</GridItem>
+      <GridItem {...horizontal.contentGrid}>{tabContent}</GridItem>
+    </GridContainer>
+  ) : (
+    <div>
+      {tabButtons}
+      {tabContent}
+    </div>
+  );
+}
+
+NavPills.defaultProps = {
+  active: 0,
+  color: "primary",
+};
+
+NavPills.propTypes = {
+  // index of the default active pill
+  active: PropTypes.number,
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      tabButton: PropTypes.string,
+      tabIcon: PropTypes.object,
+      tabContent: PropTypes.node,
+    })
+  ).isRequired,
+  color: PropTypes.oneOf([
+    "primary",
+    "warning",
+    "danger",
+    "success",
+    "info",
+    "rose",
+  ]),
+  horizontal: PropTypes.shape({
+    tabsGrid: PropTypes.object,
+    contentGrid: PropTypes.object,
+  }),
+  alignCenter: PropTypes.bool,
+};
--- client/src/components/sections/Footer.js
+++ client/src/components/sections/Footer.js
@@ -7,10 +7,10 @@ export default function Footer() {
     	<div class="container">
     		<div class="mail_section">
     			<ul>
-    				<li class="footer-logo"><img src="images/footer-logo.png" /></li>
-    				<li class="footer-logo"><img src="images/map-icon.png" /><span class="map_text">Gb road 123 london Uk</span></li>
-    				<li class="footer-logo"><img src="images/call-icon.png" /><span class="map_text">(+71) 9876543210</span></li>
-    				<li class="footer-logo"><img src="images/email-icon.png" /><span class="map_text">Demo@gmail.com</span></li>
+    				<li class="footer-logo"><img src="../../images/footer-logo.png" /></li>
+    				<li class="footer-logo"><img src="../../images/map-icon.png" /><span class="map_text">Gb road 123 london Uk</span></li>
+    				<li class="footer-logo"><img src="../../images/call-icon.png" /><span class="map_text">(+71) 9876543210</span></li>
+    				<li class="footer-logo"><img src="../../images/email-icon.png" /><span class="map_text">Demo@gmail.com</span></li>
     			</ul>
     	    </div> 
     	    <div class="footer_section_2">
@@ -21,29 +21,29 @@ export default function Footer() {
     		        </div>
     		        <div class="col-sm-6 col-md-6 col-lg-3">
     		        	<h2 class="shop_text">Useful Links</h2>
-    		        	<div class="image-icon"><img src="images/bulit-icon.png" /><span class="pet_text">Pet Dictionary</span></div>
-    				    <div class="image-icon"><img src="images/bulit-icon.png" /><span class="pet_text">Help Links</span></div>
-    				    <div class="image-icon"><img src="images/bulit-icon.png" /><span class="pet_text">Pet Attitudes</span></div>
-    				    <div class="image-icon"><img src="images/bulit-icon.png" /><span class="pet_text">Call a Doctor</span></div>
-    				    <div class="image-icon"><img src="images/bulit-icon.png" /><span class="pet_text">Help a Pet</span></div>
+    		        	<div class="image-icon"><img src="../../images/bulit-icon.png" /><span class="pet_text">Pet Dictionary</span></div>
+    				    <div class="image-icon"><img src="../../images/bulit-icon.png" /><span class="pet_text">Help Links</span></div>
+    				    <div class="image-icon"><img src="../../images/bulit-icon.png" /><span class="pet_text">Pet Attitudes</span></div>
+    				    <div class="image-icon"><img src="../../images/bulit-icon.png" /><span class="pet_text">Call a Doctor</span></div>
+    				    <div class="image-icon"><img src="../../images/bulit-icon.png" /><span class="pet_text">Help a Pet</span></div>
     		        </div>
     		        <div class="col-sm-6 col-md-6 col-lg-3">
     				    <h2 class="shop_text">Overview</h2>
-    				    <div class="image-icon"><img src="images/bulit-icon.png" /><span class="pet_text">Bacerim Dictionary</span></div>
-    				    <div class="image-icon"><img src="images/bulit-icon.png" /><span class="pet_text">Help Links</span></div>
-    				    <div class="image-icon"><img src="images/bulit-icon.png" /><span class="pet_text">bacerim Attitudes</span></div>    				
+    				    <div class="image-icon"><img src="../../images/bulit-icon.png" /><span class="pet_text">Bacerim Dictionary</span></div>
+    				    <div class="image-icon"><img src="../../images/bulit-icon.png" /><span class="pet_text">Help Links</span></div>
+    				    <div class="image-icon"><img src="../../images/bulit-icon.png" /><span class="pet_text">bacerim Attitudes</span></div>    				
     		        </div>
     			<div class="col-sm-6 col-md-6 col-lg-3">
     				<h2 class="adderess_text">Instagram</h2>
     				<div class="footer-img">
     					<div class="row">
     						<div class="col-sm-6">
-    							<div class="footer-img"><img src="images/footer-img1.png" style={{width: "100%"}} /></div>
-    							<div class="footer-img"><img src="images/footer-img2.png" style={{width: "100%"}} /></div>
+    							<div class="footer-img"><img src="../../images/footer-img1.png" style={{width: "100%"}} /></div>
+    							<div class="footer-img"><img src="../../images/footer-img2.png" style={{width: "100%"}} /></div>
     						</div>
     						<div class="col-sm-6">
-    							<div class="footer-img"><img src="images/footer-img3.png" style={{width: "100%"}} /></div>
-    							<div class="footer-img"><img src="images/footer-img4.png" style={{width: "100%"}} /></div>
+    							<div class="footer-img"><img src="../../images/footer-img3.png" style={{width: "100%"}} /></div>
+    							<div class="footer-img"><img src="../../images/footer-img4.png" style={{width: "100%"}} /></div>
     						</div>
     					</div>
     				</div>
@@ -55,10 +55,10 @@ export default function Footer() {
 	        		    <div class="col-sm-12">
 	        			    <div class="icons">
 	        				    <ul>
-	        					    <li><a href="#"><img src="images/fb-icon.png" /></a></li>
-	        					    <li><a href="#"><img src="images/twitter-icon.png" /></a></li>
-	        					    <li><a href="#"><img src="images/google-icon.png" /></a></li>
-	        					    <li><a href="#"><img src="images/linkedin-icon.png" /></a></li>
+	        					    <li><a href="#"><img src="../../images/fb-icon.png" /></a></li>
+	        					    <li><a href="#"><img src="../../images/twitter-icon.png" /></a></li>
+	        					    <li><a href="#"><img src="../../images/google-icon.png" /></a></li>
+	        					    <li><a href="#"><img src="../../images/linkedin-icon.png" /></a></li>
 	        				    </ul>
 	        			    </div>
 	        		    </div>
