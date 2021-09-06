bc84710b7b4040eca9fab0b29776f58f43eb7cf5
elsowiny <elsowiny@yahoo.com>
2021-09-06 18:17:36 -0400

    changed to use server

--- client/src/pages/AnimalSearch/SearchAnimals.js
+++ client/src/pages/AnimalSearch/SearchAnimals.js
@@ -96,6 +96,7 @@ export default function SearchAnimals() {
             setDistance(100);
         }
       };
+    //
 
     useEffect(() => {
       if(!animals){
--- client/src/pages/Animals/Animals.js
+++ client/src/pages/Animals/Animals.js
@@ -45,7 +45,7 @@ export default function Animals() {
     }
 
     }, []);
-
+//
 
     return (
         <>
